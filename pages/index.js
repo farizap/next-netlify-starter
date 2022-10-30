b88e8354df39a29db077f9f1452597739c561dae
Fariz Apriyanto <fapriyanto@alterra.id>
2022-10-30 20:51:59 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,20 @@
 import Head from 'next/head'
 import Header from '@components/Header'
-import Footer from '@components/Footer'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Great things cooming soon!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Great things cooming soon!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Stay tune
         </p>
       </main>
-
-      <Footer />
     </div>
   )
 }
